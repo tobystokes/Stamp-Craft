ffa02f1a8b651e0159c9c536999daea33778c4d7
Toby Stokes <toby@pretty.co.uk>
2017-11-03 12:34:06 +0000

    return unedited filename if stamp config setting is false

--- variables/StampVariable.php
+++ variables/StampVariable.php
@@ -5,12 +5,14 @@ class StampVariable
 {
 	public function er($fileName, $mode='file')
 	{
+    $stampOn = craft()->stamp->getSetting('stamp')===false ? false : true;
     $documentRoot = craft()->stamp->getSetting('stampPublicRoot')!=null ? craft()->stamp->getSetting('stampPublicRoot') : $_SERVER['DOCUMENT_ROOT'];
     $filePath = $documentRoot . $fileName;
-    
-    if ($fileName != '' && file_exists($filePath)) {
+
+    if (!$stampOn) return $fileName;
+    if ($stampOn && $fileName != '' && file_exists($filePath)) {
       $path_parts = pathinfo($fileName);
-      
+
 			if ($mode=='file') {
 				return $path_parts['dirname'] . '/' . $path_parts['filename'] . '.' . filemtime($filePath) . '.' . $path_parts['extension'];
 			} else if ($mode=='folder') {
