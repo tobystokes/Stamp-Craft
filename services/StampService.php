4ff8cb680721212baf92beab2175413954ceaf77
Toby Stokes <toby@pretty.co.uk>
2017-11-03 12:33:14 +0000

    Add an on/off setting

--- services/StampService.php
+++ services/StampService.php
@@ -3,12 +3,12 @@ namespace Craft;
 
 class StampService extends BaseApplicationComponent
 {
-  
+
   var $settings = array();
-  
+
 	/**
 	 * Gets a plugin setting
-   * 
+   *
 	 * @param $name String Setting name
 	 * @return mixed Setting value
 	 * @author André Elvan
@@ -17,20 +17,21 @@ class StampService extends BaseApplicationComponent
     $this->settings = $this->_init_settings();
     return $this->settings[$name];
   }
-  
+
 
 	/**
 	 * Gets Stamp settings from config
-   * 
+   *
 	 * @return array Array containing all settings
 	 * @author André Elvan
 	*/
   private function _init_settings() {
     $settings = array();
+    $settings['stamp'] = craft()->config->get('stamp');
     $settings['stampPublicRoot'] = craft()->config->get('stampPublicRoot');
-    
+
     return $settings;
   }
-  
-  
+
+
 }
